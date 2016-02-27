e09465a428232c85431d123fe0ce9fbc21825592
Matt Williams <matt@milliams.com>
2016-02-27 20:55:00 +0000

    Move functions to their own modules

---
+++ package.py
@@ -0,0 +1,33 @@
+import pathlib
+
+import pkg_resources
+
+from environment import vam_dir
+
+
+def package_dir(package: str) -> pathlib.Path:
+    return vam_dir() / package
+
+
+def get_distribution(package: str) -> pkg_resources.Distribution:
+    lib = package_dir(package) / 'lib'
+    package_dists = []
+    for py_version in lib.iterdir():  # Iterate over 'python3.4' etc.
+        if not py_version.is_dir():
+            continue
+        site_packages = py_version / 'site-packages'
+        dists = [d for d in pkg_resources.find_distributions(str(site_packages))]
+        package_dists += [d for d in dists if d.project_name == package]
+
+    if not package_dists:
+        raise Exception('Distribution not found in {}'.format(lib))
+
+    return package_dists[0]
+
+
+def get_entry_points(package: str):
+    dist = get_distribution(package)
+    entry_map = pkg_resources.get_entry_map(dist)
+    for entry_point_group, entry_points in entry_map.items():
+        for entry_point in entry_points.values():
+            yield entry_point_group, entry_point
--- vam.py
+++ vam.py
@@ -1,53 +1,13 @@
-import os
 import pathlib
 import shutil
 import subprocess
 import venv
 
 import click
-import pkg_resources
 
+from environment import home, packages
 from launcher import create_launcher_text, install_launcher
-
-
-def home() -> pathlib.Path:
-    return pathlib.Path(os.path.expanduser('~'))
-
-
-def vam_dir() -> pathlib.Path:
-    return home() / '.vam'
-
-
-def package_dir(package: str) -> pathlib.Path:
-    return vam_dir() / package
-
-
-def get_distribution(package: str) -> pkg_resources.Distribution:
-    lib = package_dir(package) / 'lib'
-    package_dists = []
-    for py_version in lib.iterdir():  # Iterate over 'python3.4' etc.
-        if not py_version.is_dir():
-            continue
-        site_packages = py_version / 'site-packages'
-        dists = [d for d in pkg_resources.find_distributions(str(site_packages))]
-        package_dists += [d for d in dists if d.project_name == package]
-
-    if not package_dists:
-        raise Exception('Distribution not found in {}'.format(lib))
-
-    return package_dists[0]
-
-
-def get_entry_points(package: str):
-    dist = get_distribution(package)
-    entry_map = pkg_resources.get_entry_map(dist)
-    for entry_point_group, entry_points in entry_map.items():
-        for entry_point in entry_points.values():
-            yield entry_point_group, entry_point
-
-
-def packages():
-    return [x for x in vam_dir().iterdir() if x.is_dir()]
+from package import package_dir, get_entry_points, get_distribution
 
 
 def pip_command(package: str) -> pathlib.Path:
